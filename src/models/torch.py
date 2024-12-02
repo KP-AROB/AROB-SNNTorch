cc8711431afc6255289a5c0d125b669b9ffb4d33
bniepce <brad.niepce@kaptios.com>
2024-12-02 16:55:37 +0100

    Add VGG16

--- src/models/torch.py
+++ src/models/torch.py
@@ -2,11 +2,55 @@ from torch import nn
 import torch.nn.functional as F
 
 
+class VGG16(nn.Module):
+    def __init__(self, n_input=3, n_output=4, in_size=224):
+        super(VGG16, self).__init__()
+        config = [64, 64, 'M', 128, 128, 'M', 256, 256, 256,
+                  'M', 512, 512, 512, 'M', 512, 512, 512, 'M']
+
+        self.n_input = n_input
+        self.n_output = n_output
+        self.in_size = in_size
+
+        self.net = self._make_layers(config)
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Dropout(0.5),
+            nn.Linear(7*7*512, 4096),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(4096, 4096),
+            nn.ReLU(),
+            nn.Linear(4096, self.n_output)
+        )
+
+    def _make_layers(self, cfg):
+        layers = []
+        in_channels = self.n_input
+
+        for x in cfg:
+            if x == 'M':
+                layers += [nn.MaxPool2d(2, 2)]
+            else:
+                layers += [nn.Conv2d(in_channels, x, kernel_size=3, padding=1),
+                           nn.BatchNorm2d(x),
+                           nn.ReLU()
+                           ]
+                in_channels = x
+        layers += [nn.Flatten()]
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.net(x)
+        x = self.classifier(x)
+        return x
+
+
 class MNET10(nn.Module):
     def __init__(self, n_input=3, n_output=4, in_size=224):
         super(MNET10, self).__init__()
 
-        self.input = n_input
+        self.n_input = n_input
         self.n_output = n_output
         self.in_size = in_size
         k_size = 3
@@ -17,10 +61,12 @@ class MNET10(nn.Module):
         self.conv2 = nn.Conv2d(16, 32, k_size)
         self.conv3 = nn.Conv2d(32, 32, k_size)
         self.conv4 = nn.Conv2d(32, 64, k_size)
+
         self.prelu = nn.PReLU()
         self.flatten = nn.Flatten()
         self.fc1 = nn.Linear(fc_size * fc_size * 64, 1024)
         self.fc2 = nn.Linear(1024, n_output)
+
         self.dropout = nn.Dropout(0.5)
         self.pool = nn.MaxPool2d(2, 2)
 
@@ -33,4 +79,4 @@ class MNET10(nn.Module):
         x = F.relu(self.fc1(x))
         x = self.dropout(x)
         x = self.fc2(x)
-        return F.softmax(x, dim=1)
+        return x
