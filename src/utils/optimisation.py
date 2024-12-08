d07999744296894367277241e119a656238c1e30
bniepce <brad.niepce@kaptios.com>
2024-12-08 15:39:16 +0100

    Edit early stopping to best val acc

--- src/utils/optimisation.py
+++ src/utils/optimisation.py
@@ -9,17 +9,17 @@ class EarlyStopping:
         self.delta = delta
         self.verbose = verbose
         self.counter = 0
-        self.best_loss = None
+        self.best_acc = None
         self.early_stop = False
 
-    def __call__(self, model: torch.nn.Module, epoch: int, val_loss: float, log_dir: str):
-        if self.best_loss is None or val_loss < self.best_loss - self.delta:
-            self.best_loss = val_loss
+    def __call__(self, model: torch.nn.Module, epoch: int, val_acc: float, log_dir: str):
+        if self.best_acc is None or val_acc < self.best_acc - self.delta:
+            self.best_acc = val_acc
             self.counter = 0
             checkpoint = {
                 'net': model.state_dict(),
                 'epoch': epoch,
-                'max_test_acc': self.best_loss
+                'max_test_acc': self.best_acc
             }
             torch.save(checkpoint, os.path.join(log_dir, 'best_model.pth'))
         else:
