a7a898067d45bf76f76538e6abab6ba8e0eefddc
bniepce <brad.niepce@kaptios.com>
2024-12-09 10:06:29 +0100

    Add encoding to training

--- src/models/jelly.py
+++ src/models/jelly.py
@@ -1,17 +1,31 @@
 from torch import nn
-from spikingjelly.activation_based import neuron, functional, surrogate, layer
+from spikingjelly.activation_based import neuron, functional, surrogate, layer, encoding
+from typing import Tuple
 
 
 class BaseJellyNet(nn.Module):
     def __init__(self,
-                 input_shape=(1, 28, 28),
-                 n_output=10,
-                 n_steps=5):
+                 input_shape: Tuple[int] = (1, 28, 28),
+                 n_output: int = 10,
+                 n_steps: int = 5,
+                 encoding_method: str = 'IF'):
+        """Constructor of BaseJellyNet Class
+
+        Args:
+            input_shape (Tuple[int], optional): The shape of the input tensor. Defaults to (1, 28, 28).
+            n_output (int, optional): The number of output neurons. Defaults to 10.
+            n_steps (int, optional): The number of timesteps to encode the input tensor. Defaults to 5.
+            encoding_method (str, optional): The neural coding method to perform. Defaults to 'poisson'.
+        """
         super().__init__()
 
         self.input_shape = input_shape
         self.n_output = n_output
         self.n_steps = n_steps
+        self.encoding_method = encoding_method
+        self.encoder = self.get_encoder()
+
+        functional.set_step_mode(self, step_mode='m')
 
     def make_layers(self, cfg, k_size=5):
         layers = []
@@ -40,13 +54,24 @@ class BaseJellyNet(nn.Module):
         self.feature_map_size = height * width * in_channels
         return nn.Sequential(*layers)
 
+    def get_encoder(self):
+        available_methods = ['IF', 'poisson']
+        if self.encoding_method not in available_methods:
+            raise ValueError(
+                'encoding_method must be of {}'.format(available_methods))
+        if self.encoding_method == 'IF':
+            return None
+        elif self.encoding_method == 'poisson':
+            return encoding.PoissonEncoder()
+
 
 class ShallowCSNN(BaseJellyNet):
     def __init__(self,
                  input_shape=(1, 28, 28),
                  n_output=10,
-                 n_steps=5):
-        super().__init__(input_shape, n_output, n_steps)
+                 n_steps=5,
+                 encoding_method='IF'):
+        super().__init__(input_shape, n_output, n_steps, encoding_method)
 
         cfg = [16, 'M', 32, 'M', 64, 'M']
         self.net = self.make_layers(cfg, 3)
@@ -71,39 +96,43 @@ class SpikingMNET10(BaseJellyNet):
     def __init__(self,
                  input_shape=(1, 224, 224),
                  n_output=10,
-                 n_steps=5):
-        super().__init__(input_shape, n_output, n_steps)
+                 n_steps=5,
+                 encoding_method='IF'):
+        super().__init__(input_shape, n_output, n_steps, encoding_method)
 
         k_size = 3
         n_input = input_shape[0]
 
         self.block1 = nn.Sequential(
             layer.Conv2d(n_input, 16, k_size),
-            neuron.LIFNode(surrogate_function=surrogate.ATan()),
             layer.BatchNorm2d(16),
+            neuron.LIFNode(surrogate_function=surrogate.ATan()),
             layer.MaxPool2d(2)
         )
 
         self.block2 = nn.Sequential(
             layer.Conv2d(16, 32, k_size),
-            neuron.LIFNode(surrogate_function=surrogate.ATan()),
             layer.BatchNorm2d(32),
+            neuron.LIFNode(surrogate_function=surrogate.ATan()),
             layer.MaxPool2d(2)
         )
 
         self.block3 = nn.Sequential(
             layer.Conv2d(32, 64, k_size),
-            neuron.LIFNode(surrogate_function=surrogate.ATan()),
             layer.BatchNorm2d(64),
+            neuron.LIFNode(surrogate_function=surrogate.ATan()),
             layer.MaxPool2d(2)
         )
 
         self.block4 = nn.Sequential(
             layer.Conv2d(64, 64, k_size),
-            neuron.LIFNode(surrogate_function=surrogate.ATan()),
             layer.BatchNorm2d(64),
+            neuron.LIFNode(surrogate_function=surrogate.ATan()),
             layer.MaxPool2d(2)
         )
+
+        self.net = nn.Sequential(
+            self.block1, self.block2, self.block3, self.block4)
 
         self.classifier = nn.Sequential(
             layer.Flatten(),
@@ -117,10 +146,64 @@ class SpikingMNET10(BaseJellyNet):
 
     def forward(self, x):
         x = x.unsqueeze(0).repeat(self.n_steps, 1, 1, 1, 1)
-        x = self.block1(x)
-        x = self.block2(x)
-        x = self.block3(x)
-        x = self.block4(x)
+        if self.encoder:
+            x = self.encoder(x)
+        x = self.net(x)
+        x = self.classifier(x)
+        x = x.mean(0)
+        return x
+
+
+class SimpleSpikingMNET(BaseJellyNet):
+    def __init__(self,
+                 input_shape=(1, 224, 224),
+                 n_output=10,
+                 n_steps=5,
+                 encoding_method='IF'):
+        super().__init__(input_shape, n_output, n_steps, encoding_method)
+
+        k_size = 3
+        n_input = input_shape[0]
+
+        self.block1 = nn.Sequential(
+            layer.Conv2d(n_input, 16, k_size),
+            layer.BatchNorm2d(16),
+            neuron.LIFNode(surrogate_function=surrogate.ATan()),
+            layer.MaxPool2d(2)
+        )
+
+        self.block2 = nn.Sequential(
+            layer.Conv2d(16, 32, k_size),
+            layer.BatchNorm2d(32),
+            neuron.LIFNode(surrogate_function=surrogate.ATan()),
+            layer.MaxPool2d(2)
+        )
+
+        self.block3 = nn.Sequential(
+            layer.Conv2d(32, 64, k_size),
+            layer.BatchNorm2d(64),
+            neuron.LIFNode(surrogate_function=surrogate.ATan()),
+            layer.MaxPool2d(2)
+        )
+
+        self.net = nn.Sequential(
+            self.block1, self.block2, self.block3)
+
+        self.classifier = nn.Sequential(
+            layer.Flatten(),
+            layer.Linear(26 * 26 * 64, 512),
+            neuron.LIFNode(surrogate_function=surrogate.ATan()),
+            layer.Dropout(0.5),
+            layer.Linear(512, self.n_output)
+        )
+
+        functional.set_step_mode(self, step_mode='m')
+
+    def forward(self, x):
+        x = x.unsqueeze(0).repeat(self.n_steps, 1, 1, 1, 1)
+        if self.encoder:
+            x = self.encoder(x)
+        x = self.net(x)
         x = self.classifier(x)
         x = x.mean(0)
         return x
